576c022b26544b021dbf7ee129a3e4e88c90f0e6
Navaneeth Rao <decodestudios.help@gmail.com>
2023-01-14 16:36:35 +0530

    redirect to window.origin

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 	function gotoAuthProvider() {
-		const redirectUrl = 'http://localhost:5173/redirect';
+		const redirectUrl = `${window.origin}/redirect`;
 		const url = data.provider.authUrl + redirectUrl;
 		window.location.href = url;
 	}
