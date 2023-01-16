587a06d2a9ab2f075499fcea6f157f8240352e98
Navaneeth Rao <decodestudios.help@gmail.com>
2023-01-16 08:39:37 +0530

    save action

--- src/routes/movie/[slug]/+page.server.ts
+++ src/routes/movie/[slug]/+page.server.ts
@@ -1,6 +1,7 @@
 import { THE_MOVIE_DB_API_KEY } from '$env/static/private';
 import type { Movie, Trailer } from '$lib/api-types';
-import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load = (async ({ params }) => {
 	const response = await fetch(
@@ -16,3 +17,15 @@ export const load = (async ({ params }) => {
 		trailers
 	};
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+	default: async ({ request, locals }) => {
+		const formData = await request.formData();
+		try {
+			await locals.pb.collection('saves').create(formData);
+		} catch (err) {
+			console.log(err);
+			throw error(500, 'Something went wrong');
+		}
+	}
+};
--- src/routes/movie/[slug]/+page.svelte
+++ src/routes/movie/[slug]/+page.svelte
@@ -12,7 +12,12 @@
 			class="rounded-lg aspect-[2/3] max-h-[700px] object-cover"
 		/>
 	</div>
-	<div class="flex-1">
+	<form class="flex-1" method="post">
+		<input type="hidden" name="movie_id" value={data.movie.id} />
+		<input type="hidden" name="movie_title" value={data.movie.title} />
+		<input type="hidden" name="movie_overview" value={data.movie.overview} />
+		<input type="hidden" name="poster_path" value={data.movie.poster_path} />
+		<input type="hidden" name="user_id" value={data.user?.id} />
 		<h2 class="text-2xl font-bold">{data.movie.title}</h2>
 		<p class="text-neutral-700 text-sm mt-2">{data.movie.release_date}</p>
 		<p class="leading-6 mt-4">{data.movie.overview}</p>
@@ -22,5 +27,5 @@
 			<span>Save</span>
 			<iconify-icon icon="material-symbols:bookmark" />
 		</button>
-	</div>
+	</form>
 </div>
