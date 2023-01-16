6bd7012ca22635d0cac91afdec0d799bd8b1b375
Navaneeth Rao <decodestudios.help@gmail.com>
2023-01-16 08:09:16 +0530

    added save butn and icons

--- src/routes/movie/[slug]/+page.svelte
+++ src/routes/movie/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	export let data: PageData;
+	import 'iconify-icon';
 </script>
 
 <div class="mt-4 flex flex-col md:flex-row gap-2">
@@ -15,5 +16,11 @@
 		<h2 class="text-2xl font-bold">{data.movie.title}</h2>
 		<p class="text-neutral-700 text-sm mt-2">{data.movie.release_date}</p>
 		<p class="leading-6 mt-4">{data.movie.overview}</p>
+		<button
+			class="px-2 py-1 rounded bg-blue-500 hover:bg-blue-600 text-white mt-4 flex items-center gap-1"
+		>
+			<span>Save</span>
+			<iconify-icon icon="material-symbols:bookmark" />
+		</button>
 	</div>
 </div>
