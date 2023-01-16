62fee703520cc38c2267c8b894465af71fb99b66
Navaneeth Rao <decodestudios.help@gmail.com>
2023-01-16 11:46:19 +0530

    inline block for a

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -10,8 +10,12 @@
 		<a href="/" class="font-bold hover:text-neutral-200">MovieFam</a>
 		<div>
 			{#if !user}
-				<a href="/login" class="px-2 py-1 rounded bg-blue-500 hover:bg-blue-600">Login</a>
-				<button class="px-2 py-1 rounded bg-white hover:bg-neutral-200 text-black">Register</button>
+				<a href="/login" class="px-2 py-1 rounded bg-blue-500 hover:bg-blue-600 inline-block"
+					>Login</a
+				>
+				<button class="px-2 py-1 rounded bg-white hover:bg-neutral-200 text-black inline-block"
+					>Register</button
+				>
 			{:else}
 				<a
 					href="/profile"
