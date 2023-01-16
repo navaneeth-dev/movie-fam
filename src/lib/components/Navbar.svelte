b908f53cd3a3f5333edf7dcd9870d966a257aa76
Navaneeth Rao <decodestudios.help@gmail.com>
2023-01-16 18:02:29 +0530

    hide profile txt on small screens for navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -8,6 +8,11 @@
 <header class="bg-neutral-900 h-14 text-white">
 	<div class="safe-area flex items-center h-full justify-between">
 		<a href="/" class="font-bold hover:text-neutral-200">MovieFam</a>
+		<input
+			type="text"
+			class="bg-transparent border border-neutral-700 px-2 py-1 rounded outline-none lg:min-w-[400px]"
+			placeholder="Search"
+		/>
 		<div>
 			{#if !user}
 				<a href="/login" class="px-2 py-1 rounded bg-blue-500 hover:bg-blue-600 inline-block"
@@ -19,10 +24,10 @@
 			{:else}
 				<a
 					href="/profile"
-					class="px-2 py-1 rounded bg-blue-500 hover:bg-blue-600 flex items-center gap-1"
+					class="px-2 py-1 rounded bg-blue-500 hover:bg-blue-600 flex items-center gap-1 h-8"
 				>
 					<iconify-icon icon="healthicons:ui-user-profile" />
-					<span>Profile</span>
+					<span class="hidden md:block">Profile</span>
 				</a>
 			{/if}
 		</div>
